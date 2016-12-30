e508fb9945a66abe068721b46a1a70a3b2479b19
Jonathan Yong <jonathanyong@Jonathans-MacBook-Pro.local>
2016-12-30 20:23:30 +0800

    Initial Commit

---
+++ dogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dogs
+//
+//  Created by Jonathan Yong on 30/12/2016.
+//  Copyright © 2016 Jonathan Yong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
